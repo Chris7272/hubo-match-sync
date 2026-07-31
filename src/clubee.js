24009bd3b70bbe603211c1fc2d77518b849a9fe6
Chris7272 <cchtuka@telenet.be>
2026-07-31 10:29:55 +0200

    Update clubee.js

--- src/clubee.js
+++ src/clubee.js
@@ -17,18 +17,4 @@ const searches = [
 
 for (const s of searches) {
   console.log(`${s}:`, html.indexOf(s));
-}import fs from "fs/promises";
-
-const html = await fs.readFile("data/liga.html", "utf8");
-
-console.log("HTML size:", html.length);
-
-const pos = html.indexOf('"games":');
-
-console.log("games found:", pos);
-
-if (pos === -1) {
-  throw new Error("games not found");
 }
-
-console.log(html.substring(pos, pos + 500));
