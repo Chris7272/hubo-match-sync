53664d05373c1236c0e808aaae8b13276af9ec2d
Chris7272 <cchtuka@telenet.be>
2026-07-31 12:48:43 +0200

    Update parse-j18.js

--- src/parse-j18.js
+++ src/parse-j18.js
@@ -1,5 +1,5 @@
 import { downloadPage } from "./download.js";
-import { parseLiga } from "./clubee.js";
+import { parseLiga } from "./parse-liga.js";
 
 const url =
   "https://www.clubee.com/handballbelgium/u18-m--982070v4/leagues/18738/seasons/220";
@@ -13,3 +13,5 @@ await parseLiga(
   "data/j18.html",
   "data/j18.json"
 );
+
+console.log("Finished.");
