953299728ffd7b09a1c9dec9fd71205e02f3e421
Chris7272 <cchtuka@telenet.be>
2026-07-31 10:27:54 +0200

    Implement search terms logging in clubee.js
    Added a list of search terms and logged their positions in the HTML.

--- src/clubee.js
+++ src/clubee.js
@@ -4,6 +4,25 @@ const html = await fs.readFile("data/liga.html", "utf8");
 
 console.log("HTML size:", html.length);
 
+const searches = [
+  '"games":',
+  '"games"',
+  'games',
+  '__NEXT_DATA__',
+  '__next_f',
+  'Sporthal Vordensteyn',
+  'venue_name',
+  'start_date'
+];
+
+for (const s of searches) {
+  console.log(`${s}:`, html.indexOf(s));
+}import fs from "fs/promises";
+
+const html = await fs.readFile("data/liga.html", "utf8");
+
+console.log("HTML size:", html.length);
+
 const pos = html.indexOf('"games":');
 
 console.log("games found:", pos);
